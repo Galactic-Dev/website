f736dfb84d6e806237f8b7a3edece03cb2b4fa7f
Galactic-Dev <devgalactic@gmail.com>
2021-04-11 11:35:38 -0500

    testing out depictions

--- pages/iSponsorBlock.js
+++ pages/iSponsorBlock.js
@@ -2,7 +2,7 @@ import Depiction from '../components/Depiction'
 
 export default function iSponsorBlock() {
 	return (
-		<Depiction name="iSponsorBlock" github="https://github.com/Galactic-Dev/iSponsorBlock" subtitle="This tweak is powered by the SponsorBlock api found"<a href="https://sponsor.ajay.app/">here</a> 
+		<Depiction name="iSponsorBlock" github="https://github.com/Galactic-Dev/iSponsorBlock" subtitle="This tweak is powered by the SponsorBlock api found"<a href="https://sponsor.ajay.app/">here</a>>
 			iSponsorBlock | Automatically skip annoying sponsorships in YouTube videos
 		</Depiction>
 	)
