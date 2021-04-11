590df04bd2db79b718efd116e18bc106e98e5d75
Galactic-Dev <devgalactic@gmail.com>
2021-04-11 11:38:06 -0500

    testing out depictions

--- pages/14PiP.js
+++ pages/14PiP.js
@@ -1,6 +1,6 @@
 import Depiction from '../components/Depiction'
 
-export default function 14PiP() {
+export default function PiP() {
         return (
                 <Depiction name="14PiP" github="https://github.com/Galactic-Dev/14PiP">
 			Bring the iOS 14 style PiP view to iOS 13
