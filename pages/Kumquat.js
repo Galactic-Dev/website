e3d606f0923f66d9dced6929fbf7d918d8a2ebdd
Galactic-Dev <devgalactic@gmail.com>
2021-05-12 19:14:35 -0500

    shhhh

---
+++ pages/Kumquat.js
@@ -0,0 +1,29 @@
+import Depiction from '../components/Depiction'
+
+export default function Kumquat() {
+        return (
+                <Depiction name="Kumquat" github="https://github.com/Galactic-Dev/Kumquat" subtitle="Customize your lockscreen music player on iOS 14.2+">
+			Features:
+<ul>
+    <li>iChoose between 4 main styles (Normal, Large, Compact, No Scrubber, and No Volume)</li>
+    <li>Have separate layouts for when you have notifications and don't have notifications</li>
+    <li>Automatically applying settings with no respring required for anything</li>
+    <li>Custom presets with the following options:</li>
+        <ul>
+        <li>Hide Route Button</li>
+        <li>Hide Artwork</li>
+        <li>Hide App Icon on Artwork</li>
+        <li>Hide Volume Bar</li>
+        <li>Hide Scrub Bar</li>
+        <li>Don't open the music app when pressed (for artwork and header text)</li>
+        <li>Hide Speaker Icons</li>
+        <li>Set Background Alpha</li>
+        <li>Custom Artwork Corner Radius</li>
+        <li>Custom Player Corner Radius</li>
+        <li>Custom player frames with absolute and relative options for header, artwork, player, volume bar, scrub bar, and transport buttons</li>
+        </ul>
+    <li>Enables scrolling text on the player</li>
+</ul>
+                </Depiction>
+        )
+}
