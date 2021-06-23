83636732459b4dc39bc2f2e5695771e55a4aa842
Galactic-Dev <devgalactic@gmail.com>
2021-06-22 19:56:33 -0500

    .

--- pages/superSecretTestPage.js
+++
@@ -1,8 +0,0 @@
-import Depiction from '../components/Depiction'
-
-export default function WhatAreYouDoingHere() {
-        return (
-                <Depiction name="WhatAreYouDoingHere">
-                </Depiction>
-        )
-}
