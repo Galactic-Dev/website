5b9696d486011b2d6c84e3671256ae13ea789441
Galactic-Dev <devgalactic@gmail.com>
2021-11-27 10:13:18 -0600

    update isb

--- pages/iSponsorBlock.js
+++ pages/iSponsorBlock.js
@@ -5,7 +5,7 @@ export default function iSponsorBlock() {
 		<Depiction name="iSponsorBlock" github="https://github.com/Galactic-Dev/iSponsorBlock" subtitle="This tweak is powered by the SponsorBlock api">
 			iSponsorBlock | Automatically skip annoying sponsorships in YouTube videos
 
-			This update fixes a crash in the most recent YouTube version.
+			This update makes iSponsorBlock functional in the most recent YouTube version.
 		</Depiction>
 	)
 }
