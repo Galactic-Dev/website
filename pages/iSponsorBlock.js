09c25f218b85d4e42c8f84f11d055e5134a10bcc
Galactic-Dev <devgalactic@gmail.com>
2022-02-09 21:42:29 -0600

    update notfinical, & fix isb changelog formatting

--- pages/iSponsorBlock.js
+++ pages/iSponsorBlock.js
@@ -4,11 +4,11 @@ export default function iSponsorBlock() {
 	return (
 		<Depiction name="iSponsorBlock" github="https://github.com/Galactic-Dev/iSponsorBlock" subtitle="This tweak is powered by the SponsorBlock api">
 			iSponsorBlock | Automatically skip annoying sponsorships in YouTube videos
-\n
-			Changelog: \n
-				- 1.0-13: Fix visual bug with sponsor segments \n
-				- 1.0-12: Fixes sponsor segments not appearing in YouTube version 17.03.2 \n
-				- 1.0-11: Fixes sponsor segments not showing in YouTube versions 16.22.5 +. Fixes iSponsorBlock menu not opening in fullscreen. Fixes donation buttons in settings.\n
+<br/>
+			Changelog: <br/>
+				- 1.0-13: Fix visual bug with sponsor segments <br/>
+				- 1.0-12: Fixes sponsor segments not appearing in YouTube version 17.03.2 <br/>
+				- 1.0-11: Fixes sponsor segments not showing in YouTube versions 16.22.5 +. Fixes iSponsorBlock menu not opening in fullscreen. Fixes donation buttons in settings. <br/>
 		</Depiction>
 	)
 }
--- repo.js
+++ repo.js
@@ -38,6 +38,7 @@ exports.packages = [
     'https://github.com/Galactic-Dev/website/releases/download/bigytminiplaye1.0-1/com.galacticdev.bigytminiplayer_1.0-1_iphoneos-arm.deb',
     'https://github.com/Galactic-Dev/website/releases/download/isponsorblock1.0-12/com.galacticdev.isponsorblock_1.0-12_iphoneos-arm.deb',	
     'https://github.com/Galactic-Dev/website/releases/download/isponsorblock1.0-13/com.galacticdev.isponsorblock_1.0-13_iphoneos-arm.deb',
+    'https://github.com/Galactic-Dev/website/releases/download/notfinical1.0.3/com.galacticdev.notfinical_1.0.3_iphoneos-arm.deb',
 ]
 
 exports.icons = {
